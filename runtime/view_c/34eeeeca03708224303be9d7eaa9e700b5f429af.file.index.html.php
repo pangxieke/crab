defc2f320d45fb332bc6b8d3e36e1ae3b6bd9831
pangxieke <pangxieke@126.com>
2016-08-02 12:55:16 +0800

    runtime

---
+++ runtime/view_c/34eeeeca03708224303be9d7eaa9e700b5f429af.file.index.html.php
@@ -0,0 +1,35 @@
+<?php /* Smarty version Smarty-3.1.15, created on 2016-08-02 12:54:42
+         compiled from "./views/site/index.html" */ ?>
+<?php /*%%SmartyHeaderCode:2917757a02792c609e4-23874952%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '34eeeeca03708224303be9d7eaa9e700b5f429af' => 
+    array (
+      0 => './views/site/index.html',
+      1 => 1470110662,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2917757a02792c609e4-23874952',
+  'function' => 
+  array (
+  ),
+  'variables' => 
+  array (
+    'hello' => 0,
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.15',
+  'unifunc' => 'content_57a02792ddf747_26299441',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_57a02792ddf747_26299441')) {function content_57a02792ddf747_26299441($_smarty_tpl) {?><!DOCTYPE html>
+<head>
+	<title>index</title>
+</head>
+<body>
+	this is crabPHP
+	<?php echo $_smarty_tpl->tpl_vars['hello']->value;?>
+
+</body>
+<?php }} ?>
